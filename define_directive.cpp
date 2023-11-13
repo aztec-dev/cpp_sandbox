06f280ce04833dbaaaffa8a76dbafb3bee0b779d
aztec-dev <aztech.dev1@gmail.com>
2023-11-13 21:03:00 +1000

    Add diameter of circle symoblic constant.

---
+++ define_directive.cpp
@@ -0,0 +1,15 @@
+/*
+* Code providing potential use cases for the #define directive in c++
+*/
+
+#include <iostream>
+#define D_CIRCLE 360
+
+using namespace std;
+
+int main () {
+
+    cout << "value of a diameter of a circle: " << D_CIRCLE << endl;
+    return 0;
+}
+
