4bd8a944f468d6bf106a0c37101f407593babd23
aztec-dev <aztech.dev1@gmail.com>
2023-11-13 23:17:46 +1000

    Add variable tests.

--- variables.cpp
+++ variables.cpp
@@ -1,3 +1,32 @@
 /*
 * C++ code demonstrating the use of variables.
 */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int initInt {1};
+    int uninitInt;
+    string uninitString;
+
+    /*
+    * What are some potential use cases for uninitalzied variables?
+    */
+
+    cout <<  "This is an initialized Int: " << initInt << endl;
+    cout << "This is an uninitalized Int: " << uninitInt << endl;  // This should display a semi-random number
+    cout << "This is an uninitalized String: " << uninitString << endl;  // This will display an empty string
+
+    if (uninitString == "")
+    {
+        // Set unintString to contain a word
+        uninitString = "Hello World :)";
+        cout << "This is an uninitalized String: " << uninitString << endl;
+    }
+
+    
+
+    return 0;
+}
