f495dd209a3b26bae3176f4c5cfc41de78012d77
aztec-dev <aztech.dev1@gmail.com>
2023-11-14 20:07:43 +1000

    Remove unused variable

--- variables.cpp
+++ variables.cpp
@@ -15,7 +15,7 @@ int main() {
     /*
     * What are some potential use cases for uninitalzied variables?
     */
-
+   initInt = 1 + 1;
     cout <<  "This is an initialized Int: " << initInt << endl;
     cout << "This is an uninitalized Int: " << uninitInt << endl;  // This should display a semi-random number
     cout << "This is an uninitalized String: " << uninitString << endl;  // This will display an empty string
@@ -32,6 +32,15 @@ int main() {
 
     cout << "Testing numerical limits: \n";
     cout << "Min unsigned value: " << numeric_limits<unsigned>::lowest();
+
+    /*
+    * Casting test - changing variable types
+    */
+
+   float myFloat {3.11901};
+   cout << "Float: " << myFloat << endl;
+   int int1 {static_cast<int>(myFloat)};
+   cout << "Float conversion: " << int1 << endl;
 
     return 0;
 }
